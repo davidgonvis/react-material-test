8b246863ae0ba5134ab78b8d9b2c2bff08bc4cc9
davidgonvis <galvisd153@gmail.com>
2021-06-12 05:07:37 +0800

    group id

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Container } from '@material-ui/core';
 import InputGroup from './components/InputGroup';
 
 function App() {
+  
   return (
     <Container maxWidth="lg">
-      <InputGroup></InputGroup>
-      <InputGroup></InputGroup>
-      <InputGroup></InputGroup>
-      <InputGroup></InputGroup>
-      <InputGroup></InputGroup>
+      <InputGroup group_id="1"></InputGroup>
+      <InputGroup group_id="2"></InputGroup>
+      <InputGroup group_id="3"></InputGroup>
+      <InputGroup group_id="4"></InputGroup>
+      <InputGroup group_id="5"></InputGroup>
     </Container>
   );
 }
